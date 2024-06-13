3980d122ad3d32a72eca8801134873c530bc25be
BomJak <denis250597@yandex.ru>
2024-06-13 13:48:37 +0300

    KS-1-2-make-value-for-count-workers

---
+++ src/main/kotlin/lesson1/lesson1_2.kt
@@ -0,0 +1,15 @@
+package lesson1
+
+fun main() {
+    val countOrder: Int = 75
+    val textGratitudeOrder: String = "Something about task"
+
+    println(countOrder)
+    println(textGratitudeOrder)
+
+    var countWorkersOS: Int = 2000
+    //println(countWorkersOS)
+
+    countWorkersOS -= 1
+    println(countWorkersOS)
+}
