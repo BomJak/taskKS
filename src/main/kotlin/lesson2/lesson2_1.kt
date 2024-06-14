cb7fa4a9645e271a33a69889cca4231e0a5b322c
BomJak <denis250597@yandex.ru>
2024-06-15 00:30:08 +0300

    KS-2-1-announce-variable-to-score

--- src/main/kotlin/lesson2/lesson2_1.kt
+++ src/main/kotlin/lesson2/lesson2_1.kt
@@ -1,6 +1,10 @@
 package lesson2
 
 fun main(){
-    var arfMean: Float = (3+4+3+5)/4f
+    val firstScore: Int = 3
+    val secondScore: Int = 4
+    val thirdScore: Int = 3
+    val fourthScore: Int = 5
+    var arfMean: Float = (firstScore+secondScore+thirdScore+fourthScore)/4f
     println(String.format("%.2f",arfMean))
 }
