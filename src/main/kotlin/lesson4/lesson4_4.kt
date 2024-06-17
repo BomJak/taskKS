be3e91b7635c2388f4664d5221c7ccc9774dcb2b
BomJak <denis250597@yandex.ru>
2024-06-17 17:43:34 +0300

    KS-4-4-using-only-one-varible-for-check-day

--- src/main/kotlin/lesson4/lesson4_4.kt
+++ src/main/kotlin/lesson4/lesson4_4.kt
@@ -3,17 +3,14 @@ package lesson4
 fun main() {
     var curDays = 5
 
-    var workoutArms: Boolean = (curDays % 2 != 0)
-    var workoutLegs: Boolean = (curDays % 2 == 0)
-    var workoutBack: Boolean = (curDays % 2 == 0)
-    var workoutPress: Boolean = (curDays % 2 != 0)
+    val isEven: Boolean = (curDays % 2 == 0)
 
     println(
         """
-        Упражнения для рук:    $workoutArms
-        Упражнения для ног:    $workoutLegs
-        Упражнения для спины:  $workoutBack
-        Упражнения для пресса: $workoutPress
+        Упражнения для рук:    ${!isEven}
+        Упражнения для ног:    $isEven
+        Упражнения для спины:  $isEven
+        Упражнения для пресса: ${!isEven}
     """.trimMargin("$")
     )
 }
