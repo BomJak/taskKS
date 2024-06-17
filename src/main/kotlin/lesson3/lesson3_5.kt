30c9b7713b5dc16fd4894661672a7fe17a6c408c
BomJak <denis250597@yandex.ru>
2024-06-17 11:20:12 +0300

    KS-5-5-using-String.split-for-parsing-in-log

--- src/main/kotlin/lesson3/lesson3_5.kt
+++ src/main/kotlin/lesson3/lesson3_5.kt
@@ -2,9 +2,12 @@ package lesson3
 
 fun main() {
     val inLogChess = "D2-D4;0"
-    val inMoveLog: String = inLogChess.substring(0..1)
-    val outMoveLog: String = inLogChess.substring(3..4)
-    val countMoveLog: String = inLogChess.substringAfter(';')
+
+    val parsingLog =inLogChess.split("-",";")
+
+    val inMoveLog = parsingLog[0]
+    val outMoveLog = parsingLog[1]
+    val countMoveLog = parsingLog[2]
 
     println(inMoveLog)
     println(outMoveLog)
