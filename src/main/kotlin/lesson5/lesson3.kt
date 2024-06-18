935e3003c3f4b060a7d06e547ade4b8ce72e8de9
BomJak <denis250597@yandex.ru>
2024-06-18 22:41:29 +0300

    KS-5-3-random-value-from-winning-value

--- src/main/kotlin/lesson5/lesson3.kt
+++ src/main/kotlin/lesson5/lesson3.kt
@@ -1,7 +1,5 @@
 package lesson5
 
-const val FIRST_WIN_VAL = 1
-const val SECOND_WIN_VAL = 4
 
 const val MIN_VAL = 0
 const val MAX_VAL = 42
@@ -9,6 +7,9 @@ const val MAX_VAL = 42
 fun main() {
     val firstIn: Int
     val secondIn: Int
+
+    val firstWinValue = (MIN_VAL..MAX_VAL).random()
+    val secontWinValue =(MIN_VAL..MAX_VAL).random()
 
     println("Для участие введите два значения в диапозоне От 0 До 42")
 
@@ -21,16 +22,18 @@ fun main() {
         println("Ошибка ввода")
     } else {
         when {
-            ((firstIn == FIRST_WIN_VAL || firstIn == SECOND_WIN_VAL) && (secondIn == FIRST_WIN_VAL || secondIn == SECOND_WIN_VAL)) -> println(
+            ((firstIn == firstWinValue || firstIn == secontWinValue) &&
+                    (secondIn == firstWinValue || secondIn == secontWinValue)) -> println(
                 "Поздравляем! Вы выиграли главный приз!"
             )
 
-            ((firstIn != FIRST_WIN_VAL && firstIn != SECOND_WIN_VAL) && (secondIn != FIRST_WIN_VAL && secondIn != SECOND_WIN_VAL)) -> println(
+            ((firstIn != firstWinValue && firstIn != secontWinValue) &&
+                    (secondIn != firstWinValue && secondIn != secontWinValue)) -> println(
                 "Неудача!"
             )
 
             else -> println("Вы выиграли утешительный приз!")
         }
-        println("Победные номера $FIRST_WIN_VAL и $SECOND_WIN_VAL")
+        println("Победные номера $firstWinValue и $secontWinValue")
     }
 }
